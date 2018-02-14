0b97e612444494524ad400581d0f02d9b531ffbe
SkyN3ss <skynessj@gmail.com>
2018-02-13 21:52:03 -0400

    Profile Pics

--- app/front/src/views/dashboard.vue
+++ app/front/src/views/dashboard.vue
@@ -95,7 +95,7 @@
                             </v-btn>
                             <v-list>
                                 <v-list-tile>
-                                    <v-list-tile-title>asd</v-list-tile-title>
+                                    <v-list-tile-title>Crear Nuevo Post</v-list-tile-title>
                                 </v-list-tile>
                             </v-list>
                         </v-menu>
